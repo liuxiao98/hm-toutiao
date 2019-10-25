bd83851f50f2baa5963f39d53293f8db090f8154
liuxiao98 <you@example.com>
2019-10-25 15:08:35 +0800

    项目前的些许准备工作

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,16 +1,15 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
 
 }
 </script>
 
-<style lang="less">
+<style>
 
 </style>
